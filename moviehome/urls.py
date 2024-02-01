6068001ca064e4eabcbc1d7ae4bb3cbfec9d8982
a-kumar5 <ayush.1306.kumar@gmail.com>
2024-02-01 23:35:39 +0530

    Added image support for apis

--- moviehome/urls.py
+++ moviehome/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from movies.views import MovieViewSet, ActionViewSet, ComedyViewSet
 
@@ -28,3 +30,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- movies/serializers.py
+++ movies/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import MovieData
 
 class MovieSerializers(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None, use_url=True)
     class Meta:
         model = MovieData
-        fields = ['id', 'name', 'duration', 'rating', 'genre']
+        fields = ['id', 'name', 'duration', 'rating', 'genre', 'image']
